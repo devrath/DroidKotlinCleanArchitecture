6d6077d95e4e6b7c005f5348e6102d1b1c271a5f
Devrath <devrath.dev595@gmail.com>
2021-05-08 22:33:19 +0530

    v

--- app/src/main/java/com/example/myapplication/models/Movie.kt
+++ app/src/main/java/com/example/myapplication/models/Movie.kt
@@ -2,6 +2,11 @@ package com.example.myapplication.models
 
 import com.example.myapplication.core.extension.empty
 
+/**
+ * Movie data class for each movie node
+ * @param id ---- > Identifier of the movie
+ * @param poster -> Url that has a image stored in the server
+ */
 data class Movie(val id: Int, val poster: String) {
     companion object {
         val empty = Movie(0, String.empty())
